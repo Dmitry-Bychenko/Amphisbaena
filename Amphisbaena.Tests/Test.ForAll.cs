03f041f71d90045ea0ad9a8d140a06f1f66e6a32
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-01-17 00:02:54 +0300

    ForAll (stating)

---
+++ Amphisbaena.Tests/Test.ForAll.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amphisbaena.Tests {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// ForAll tests
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  [TestClass]
+  public class TestForAll {
+    #region Public
+
+    /// <summary>
+    /// Sum Of Squares with a help of ForAll 
+    /// </summary>
+    [TestMethod]
+    public async Task ForAllSumOfSquares() {
+      long[] data = Enumerable
+        .Range(1, 1000)
+        .Select(i => (long) i)
+        .ToArray();
+
+      long expected = data
+        .Sum(item => item * item);
+
+      ConcurrentDictionary<long, long> dict = new ConcurrentDictionary<long, long>();
+
+      await data
+        .ToChannelReader()
+        .ForAll((item) => dict.TryAdd(item, item * item), Environment.ProcessorCount, default);
+
+      long actual = dict.Values.Sum(item => item);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    #endregion Public
+  }
+
+}
