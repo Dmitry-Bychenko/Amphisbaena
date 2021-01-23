7d80c6bc4601052574524763b95ed69242ab8168
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-01-23 23:06:59 +0300

    SelectMany

--- Amphisbaena/Linq/Amphisbaena.Linq.Select.cs
+++ Amphisbaena/Linq/Amphisbaena.Linq.Select.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
@@ -86,6 +87,98 @@ namespace Amphisbaena.Linq {
     /// </summary>
     public static ChannelReader<T> Select<T, S>(this ChannelReader<S> reader, Func<S, T> map) =>
       Select(reader, map, default);
+
+    /// <summary>
+    /// Select Many
+    /// </summary>
+    public static ChannelReader<T> SelectMany<T, S>(this ChannelReader<S> reader, 
+                                                         Func<S, long, ChannelReader<T>> selector,
+                                                         ChannelParallelOptions options) {
+      if (reader is null)
+        throw new ArgumentNullException(nameof(reader));
+      if (selector is null)
+        throw new ArgumentNullException(nameof(selector));
+
+      ChannelParallelOptions op = options is null
+        ? new ChannelParallelOptions()
+        : options.Clone();
+
+      op.CancellationToken.ThrowIfCancellationRequested();
+
+      Channel<T> result = op.CreateChannel<T>();
+
+      Task.Run(async () => {
+        long index = -1;
+
+        await foreach(S item in reader.ReadAllAsync(op.CancellationToken).ConfigureAwait(false)) {
+          index += 1;
+
+          ChannelReader<T> inner = selector(item, index);
+
+          if (inner is null)
+            continue;
+
+          await foreach(T rec in inner.ReadAllAsync(op.CancellationToken).ConfigureAwait(false)) {
+            await result.Writer.WriteAsync(rec, op.CancellationToken).ConfigureAwait(false);
+          }
+        }
+
+        result.Writer.TryComplete();
+      }, op.CancellationToken);
+
+      return result.Reader;
+    }
+
+    /// <summary>
+    /// Select Many
+    /// </summary>
+    public static ChannelReader<T> SelectMany<T, S>(this ChannelReader<S> reader,
+                                                         Func<S, long, ChannelReader<T>> selector) =>
+      SelectMany(reader, selector, default);
+
+    /// <summary>
+    /// Select Many
+    /// </summary>
+    public static ChannelReader<T> SelectMany<T, S>(this ChannelReader<S> reader,
+                                                         Func<S, ChannelReader<T>> selector,
+                                                         ChannelParallelOptions options) {
+      if (reader is null)
+        throw new ArgumentNullException(nameof(reader));
+      if (selector is null)
+        throw new ArgumentNullException(nameof(selector));
+
+      ChannelParallelOptions op = options is null
+        ? new ChannelParallelOptions()
+        : options.Clone();
+
+      op.CancellationToken.ThrowIfCancellationRequested();
+
+      Channel<T> result = op.CreateChannel<T>();
+
+      Task.Run(async () => {
+        await foreach (S item in reader.ReadAllAsync(op.CancellationToken).ConfigureAwait(false)) {
+          ChannelReader<T> inner = selector(item);
+
+          if (inner is null)
+            continue;
+
+          await foreach (T rec in inner.ReadAllAsync(op.CancellationToken).ConfigureAwait(false)) {
+            await result.Writer.WriteAsync(rec, op.CancellationToken).ConfigureAwait(false);
+          }
+        }
+
+        result.Writer.TryComplete();
+      }, op.CancellationToken);
+
+      return result.Reader;
+    }
+
+    /// <summary>
+    /// Select Many
+    /// </summary>
+    public static ChannelReader<T> SelectMany<T, S>(this ChannelReader<S> reader,
+                                                         Func<S, ChannelReader<T>> selector) =>
+      SelectMany(reader, selector, default);
 
     #endregion Public
   }
