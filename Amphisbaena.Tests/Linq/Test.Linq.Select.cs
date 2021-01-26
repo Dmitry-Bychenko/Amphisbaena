08b133213eba648dfe94f0261d83316623319132
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-01-27 00:49:48 +0300

    Skip

---
+++ Amphisbaena.Tests/Linq/Test.Linq.Select.cs
@@ -0,0 +1,41 @@
+﻿using Amphisbaena.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amphisbaena.Tests.Linq {
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Min And Max
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  [TestCategory("Linq.Select")]
+  [TestClass]
+  public sealed class SelectTest {
+    
+    [TestMethod("SelectMany")]
+    public async Task SelectManyTest() {
+      int[][] data = new int[][] {
+        new int[] { 1, 2, 3 },
+        new int[] { 4 },
+        new int[] { 5, 6, 7, 8 },
+        new int[] { 9, 10},
+      };
+
+      int expected = data
+        .Select(line => line.Sum())
+        .Sum();
+
+      int actual = await data
+        .ToChannelReader()
+        .SelectMany(line => line.ToChannelReader())
+        .Aggregate((s, a) => s + a);
+
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
---
+++ Amphisbaena.Tests/Linq/Test.Linq.SkipAndTake.cs
@@ -0,0 +1,36 @@
+﻿using Amphisbaena.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amphisbaena.Tests.Linq {
+
+  [TestCategory("Linq.SkipAndTake")]
+  [TestClass]
+  public class SkipAndTakeTest {
+    [TestMethod("Skip And Take")]
+    public async Task SimpleStatistics() {
+      int[] data = Enumerable
+        .Range(0, 100)
+        .ToArray();
+
+      int expected = data
+        .SkipWhile(x => x < 10)
+        .TakeWhile(x => x < 50)
+        .Skip(3)
+        .Take(10)
+        .Sum();
+
+      int actual = await data
+        .ToChannelReader()
+        .SkipWhile(x => x < 10)
+        .TakeWhile(x => x < 50)
+        .Skip(3)
+        .Take(10)
+        .Aggregate((s, a) => s + a);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+  }
+}
