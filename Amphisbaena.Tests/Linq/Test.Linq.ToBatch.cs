9f4bfcd4ef5e9983e3e55d3b8a93760778029f71
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-01-27 09:04:01 +0300

    ToBatch Test

---
+++ Amphisbaena.Tests/Linq/Test.Linq.ToBatch.cs
@@ -0,0 +1,26 @@
+﻿using Amphisbaena.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amphisbaena.Tests.Linq {
+
+  [TestCategory("Linq.Statistics")]
+  [TestClass]
+  public class ToBatchStatistics {
+    [TestMethod("Simple Batch")]
+    public async Task DoBatch() {
+      int[] data = Enumerable
+        .Range(0, 100)
+        .ToArray();
+
+      int[] actual = await data
+        .ToChannelReader()
+        .ToBatch((batch, item, index) => batch.Count < 4)
+        .Skip(2)
+        .FirstOrDefault();
+
+      Assert.IsTrue(new int[] { 8, 9, 10, 11}.SequenceEqual(actual));
+    }
+  }
+}
